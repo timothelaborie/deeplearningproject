d95206cb7f3a011be2961093f04d11785f689e5c
tristangirard <82379010+tristangirard@users.noreply.github.com>
2022-11-20 21:45:34 +0100

    Update train_test.py

--- refactor/train_test.py
+++ refactor/train_test.py
@@ -5,8 +5,8 @@ from torch.nn import MultiLabelSoftMarginLoss
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import StepLR
-from torch.utils.data import DataLoader
 from deepfool import deepfool
+from utils import progress_bar
 
 
 def mixup_data(x, y, device, alpha=1.0):
@@ -22,61 +22,86 @@ def mixup_criterion(criterion, pred, y_a, y_b, lam):
     return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
 
-def train(model, device, train_loader, optimizer, epoch, specificity=""):
+def train(model, device, train_loader, optimizer, specificity=""):
     model.train()
+    train_loss = 0
+    correct = 0
+    total = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         if specificity == "mixup":
+            # Source : https://github.com/facebookresearch/mixup-cifar10
             inputs, targets_a, targets_b, lam = mixup_data(data, target, device=device, alpha=1.0)
             inputs, targets_a, targets_b = map(Variable, (inputs, targets_a, targets_b))
             outputs = model(inputs)
             loss = mixup_criterion(MultiLabelSoftMarginLoss(), outputs, targets_a, targets_b, lam)
             loss.backward()
             optimizer.step()
-            # train_loss += loss.data[0]
-            # _, predicted = torch.max(outputs.data, 1)
-            # total += targets.size(0)
-            # correct += (lam * predicted.eq(targets_a.data).cpu().sum().float() + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
+            train_loss += loss.data[0]
+            _, predicted = torch.max(outputs.data, 1)
+            total += target.size(0)
+            correct += (lam * predicted.eq(targets_a.data).cpu().sum().float() + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
         else:
             output = model(data)
             loss = MultiLabelSoftMarginLoss()(output, target)
             loss.backward()
             optimizer.step()
-        # if batch_idx % 1000 == 0:
-        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.item() / train_loader.batch_size))
+            train_loss += loss.item()
+            predictions = output.argmax(dim=1, keepdim=True)
+            correct += predictions.eq(target.argmax(dim=1, keepdim=True).view_as(predictions)).sum().item()
+            total += target.size(0)
+        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / total, 100. * correct / total, correct, total))
 
 
-def test(model, device, test_loader):
+def evaluate(model, device, data_loader, verbose=True):
     model.eval()
-    test_loss = 0
+    loss = 0
     correct = 0
+    total = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for batch_idx, (data, target) in enumerate(data_loader):
             data, target = data.to(device), target.to(device)
             output = model(data)
-            # sum up batch loss
-            test_loss += MultiLabelSoftMarginLoss()(output, target).item()
-            # get the index of the max log-probability
+            loss += MultiLabelSoftMarginLoss()(output, target).item()
             predictions = output.argmax(dim=1, keepdim=True)
-            # target is one-hot encoded, so argmax to get the index
             target = target.argmax(dim=1, keepdim=True)
             correct += predictions.eq(target.view_as(predictions)).sum().item()
-    test_loss /= len(test_loader.dataset)
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.5f}%)'.format(test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+            total += target.size(0)
+            if verbose:
+                progress_bar(batch_idx, len(data_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (loss / total, 100. * correct / total, correct, total))
+    return correct / total, loss / total
 
 
-def full_training(model, train_dataset, test_dataset, epochs, batch_size, learning_rate, device, specificity=""):
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+def full_training(model, train_loader, val_loader, hyperparameters, device, specificity=""):
+    optimizer = optim.Adam(model.parameters(), lr=hyperparameters["learning_rate"])
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-    for epoch in range(epochs):
-        print("Epoch {}/{}".format(epoch, epochs))
-        train(model, device, train_loader, optimizer, epoch, specificity=specificity)
-        test(model, device, train_loader)
+    for epoch in range(hyperparameters["epochs"]):
+        print("Epoch {}/{}".format(epoch, hyperparameters["epochs"]))
+        print("Training ...")
+        train(model, device, train_loader, optimizer, specificity=specificity)
+        print("Evaluation on the validation set ...")
+        evaluate(model, device, val_loader)
         scheduler.step()
-    deepfool_score(model, device, test_loader)
+        print("\n")
+
+
+def score_report(model, device, val_loader, test_loader, blurred_test_loader):
+    report = [['type', 'accuracy', 'loss', 'deep_fool']]
+    print("Final report :")
+    val_accuracy, val_loss = evaluate(model, device, val_loader, verbose=False)
+    val_df_score = deepfool_score(model, device, val_loader)
+    report.append(["val", val_accuracy, val_loss, val_df_score])
+    print("\tPerformance on the validation set - acc. : {}, loss : {}, DeepFool score : {}".format(val_accuracy, val_loss, val_df_score))
+    test_accuracy, test_loss = evaluate(model, device, test_loader, verbose=False)
+    test_df_score = deepfool_score(model, device, test_loader)
+    report.append(["test", test_accuracy, test_loss, test_df_score])
+    print("\tPerformance on the testing set - acc. : {}, loss : {}, DeepFool score : {}".format(test_accuracy, test_loss, test_df_score))
+    blurred_test_accuracy, blurred_test_loss = evaluate(model, device, blurred_test_loader, verbose=False)
+    blurred_test_df_score = deepfool_score(model, device, blurred_test_loader)
+    report.append(["blurred_test", blurred_test_accuracy, blurred_test_loss, blurred_test_df_score])
+    print("\tPerformance on the blurred testing set - acc. : {}, loss : {}, DeepFool score : {}".format(blurred_test_accuracy, blurred_test_loss, blurred_test_df_score))
+    return report
 
 
 def deepfool_score(model, device, test_loader):
@@ -93,10 +118,8 @@ def deepfool_score(model, device, test_loader):
             minimal_perturbation = data[0]
             # calculate the norm of the perturbation
             norm = np.linalg.norm(minimal_perturbation)
-            # print(norm)
             norms.append(norm)
         batches_done += 1
         if batches_done % 50 == 0:
             break
-    print('DeepFool - Average norm of perturbation needed: {:.5f}'.format(np.mean(norms)))
     return np.mean(norms)
