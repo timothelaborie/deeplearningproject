364d0dfdb0847e331ef92c426e7aadf3872a1bf9
tristangirard <82379010+tristangirard@users.noreply.github.com>
2022-11-20 21:45:55 +0100

    Update utils.py

--- refactor/utils.py
+++ refactor/utils.py
@@ -1,9 +1,13 @@
 import os
+import sys
+import time
+
 import cv2
 from torch.utils.data import DataLoader
 import torch
 from torchvision import datasets
 import numpy as np
+import csv
 
 
 def show_cuda_info():
@@ -17,6 +21,10 @@ def dir_exists(dir_name):  # Returns whether the given name is an existing direc
     return os.path.isdir(dir_name)
 
 
+def file_exists(file_name):  # Returns whether the given name is an existing file
+    return os.path.isfile(file_name)
+
+
 def create_directory(directory_name):  # Creates a new directory given the name if possible
     try:
         os.mkdir(directory_name)
@@ -24,29 +32,50 @@ def create_directory(directory_name):  # Creates a new directory given the name 
         pass
 
 
-def download_datasets(verbose=False):
+def write_csv(file_name, data):
+    with open(file_name, 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        for row in data:
+            writer.writerow(row)
+
+
+def read_csv(file_name):
+    with open(file_name, 'r') as f:
+        csv_reader = csv.reader(f)
+        for row in csv_reader:
+            print(row)
+
+
+DATASETS_CALLS = {"mnist": datasets.MNIST, "fashionmnist": datasets.FashionMNIST, "cifar10": datasets.CIFAR10, "cifar100": datasets.CIFAR100}
+DATASETS_TRAIN_SPLITS = {"mnist": [50000, 10000], "fashionmnist": [50000, 10000], "cifar10": [40000, 10000], "cifar100": [40000, 10000]}
+
+
+def download_dataset(dataset_name):
+    assert dataset_name in DATASETS_CALLS.keys(), "Dataset {} is not available".format(dataset_name)
     create_directory("./datasets")
-    for dataset_name, dataset_call in [("mnist", datasets.MNIST), ("fashionmnist", datasets.FashionMNIST), ("cifar10", datasets.CIFAR10), ("cifar100", datasets.CIFAR100)]:
+    if dir_exists("./datasets/{}".format(dataset_name)):
+        print("Dataset {} is already stored on disk".format(dataset_name))
+    else:
+        print("Downloading dataset {}".format(dataset_name))
+        dataset_call = DATASETS_CALLS[dataset_name]
         dir_name = "./datasets/{}".format(dataset_name)
         create_directory(dir_name)
         for train, split_name in [(True, "train"), (False, "test")]:
             dataset = dataset_call('../data', train=train, download=True)
             data = np.array(dataset.data)
             labels = np.array(dataset.targets)
-            if verbose:
-                print("Dataset : {}, split : {}".format(dataset_name, split_name))
-                print("\tData shape : {}".format(data.shape))
-                print("\tLabel shape : {}".format(labels.shape))
-                print("\tNumber of labels : {}".format(labels.max() + 1))
-            # convert labels to one-hot
+            print("\tSplit : {}".format(split_name))
+            print("\t\tData shape : {}".format(data.shape))
+            print("\t\tLabel shape : {}".format(labels.shape))
+            print("\t\tNumber of labels : {}".format(labels.max() + 1))
             one_hot_labels = np.zeros((labels.size, labels.max() + 1))
-            one_hot_labels[np.arange(labels.size), labels] = 1
+            one_hot_labels[np.arange(labels.size), labels] = 1  # convert labels to one-hot
             np.save("{}/{}_features.npy".format(dir_name, split_name), data)
             np.save("{}/{}_labels.npy".format(dir_name, split_name), one_hot_labels)
 
 
-def load_dataset(name, train, specificity=""):
-    split = "train" if train else "test"
+def load_dataset(name, train_val, specificity=""):
+    split = "train" if train_val else "test"
     if specificity == "":  # Standard dataset
         x = np.load("./datasets/{}/{}_features.npy".format(name, split))
         y = np.load("./datasets/{}/{}_labels.npy".format(name, split))
@@ -60,23 +89,110 @@ def load_dataset(name, train, specificity=""):
         x = x.reshape(x.shape[0], 1, 28, 28)
     else:  # name.startswith("cifar")
         x = x.reshape(x.shape[0], 3, 32, 32)
-    return torch.utils.data.TensorDataset(torch.from_numpy(x).float(), torch.from_numpy(y).float())
+    dataset = torch.utils.data.TensorDataset(torch.from_numpy(x).float(), torch.from_numpy(y).float())
+    if train_val:
+        return torch.utils.data.random_split(dataset, DATASETS_TRAIN_SPLITS[name])  # train_set, val_set
+    else:
+        return dataset
 
 
 def blur_images(dataset_name):
+    assert dataset_name in DATASETS_CALLS.keys(), "Dataset {} is not available".format(dataset_name)
     create_directory("./augmented_datasets")
     dir_name = "./augmented_datasets/{}".format(dataset_name)
-    create_directory(dir_name)
-    for split_name in ["train", "test"]:
-        x = np.load("./datasets/{}/{}_features.npy".format(dataset_name, split_name))
-        y = np.load("./datasets/{}/{}_labels.npy".format(dataset_name, split_name))
-        # blur the images using gaussian blur
-        for i in range(x.shape[0]):
-            blurred = cv2.GaussianBlur(x[i], (5, 5), 0)
-            # fig, ax = plt.subplots(1,2)
-            # ax[0].imshow(x[i])
-            # ax[1].imshow(blurred)
-            x[i] = blurred
-        # save the blurred images
-        np.save("{}/blurred_{}_features.npy".format(dir_name, split_name), x)
-        np.save("{}/blurred_{}_labels.npy".format(dir_name, split_name), y)
+    if dir_exists(dir_name):
+        print("Blurred {} dataset is already stored on disk".format(dataset_name))
+    else:
+        print("Blurring dataset {}".format(dataset_name))
+        create_directory(dir_name)
+        for split_name in ["train", "test"]:
+            x = np.load("./datasets/{}/{}_features.npy".format(dataset_name, split_name))
+            y = np.load("./datasets/{}/{}_labels.npy".format(dataset_name, split_name))
+            # blur the images using gaussian blur
+            for i in range(x.shape[0]):
+                blurred = cv2.GaussianBlur(x[i], (5, 5), 0)
+                # fig, ax = plt.subplots(1,2)
+                # ax[0].imshow(x[i])
+                # ax[1].imshow(blurred)
+                x[i] = blurred
+            # save the blurred images
+            np.save("{}/blurred_{}_features.npy".format(dir_name, split_name), x)
+            np.save("{}/blurred_{}_labels.npy".format(dir_name, split_name), y)
+
+
+_, term_width = 0, 180  # os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+TOTAL_BAR_LENGTH = 86.
+last_time = time.time()
+begin_time = last_time
+
+
+# Source : https://github.com/facebookresearch/mixup-cifar10
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+# Source : https://github.com/facebookresearch/mixup-cifar10
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
