a65cb825257af05b6f4435bdcb98fb5e6ab57187
tristangirard <82379010+tristangirard@users.noreply.github.com>
2022-11-23 15:55:42 +0100

    Report computes avg&std over seeds
    (correct wrong commit)

--- refactor/utils.py
+++ refactor/utils.py
@@ -1,176 +1,195 @@
+import os
+import sys
+import time
 import numpy as np
-from torch import optim
-from torch.autograd import Variable
-from torch.nn import MultiLabelSoftMarginLoss
 import torch
-import torch.nn as nn
-from torch.optim.lr_scheduler import StepLR
-from data import DATASET_IMAGE_CHN, DATASET_IMAGE_DIM
-from deepfool import deepfool
-from utils import progress_bar, mixup_criterion, mixup_data
-from model import vae_loss_function
+import csv
 
 
-def train(model, device, train_loader, dataset_name, optimizer, specificity="", vae_model=None, hyperparameters=None):
-    model.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        if specificity == "":
-            # Standard training
-            output = model(data)
-            loss = MultiLabelSoftMarginLoss()(output, target)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-            predictions = output.argmax(dim=1, keepdim=True)
-            correct += predictions.eq(target.argmax(dim=1, keepdim=True).view_as(predictions)).sum().item()
-            total += target.size(0)
-        elif specificity in ["mixup", "manifold_mixup"]:
-            # Mixup on intermediate representation (initial mixup is a special case)
-            layer_mix = 0 if specificity == "mixup" else None
-            outputs, targets_a, targets_b, lam = model(data, target=target, mixup_hidden=True, layer_mix=layer_mix)
-            loss = mixup_criterion(MultiLabelSoftMarginLoss(), outputs, targets_a, targets_b, lam)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += target.size(0)
-            _, targets_a = torch.max(targets_a, 1)
-            _, targets_b = torch.max(targets_b, 1)
-            correct += (lam * predicted.eq(targets_a.data).cpu().sum().float() + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
-        elif specificity == "mixup_vae":
-            # Mixup on the latent codes
-            assert vae_model is not None, "No VAE model has been provided"
-            inputs, targets_a, targets_b, lam = mixup_data(data, target, device=device, alpha=1.0)
-            inputs, targets_a, targets_b = map(Variable, (inputs, targets_a, targets_b))
-            inputs = vae_model.decoder(inputs).to(device).view(hyperparameters["batch_size"], DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name])
-            outputs = model(inputs)
-            loss = mixup_criterion(MultiLabelSoftMarginLoss(), outputs, targets_a, targets_b, lam)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += target.size(0)
-            _, targets_a = torch.max(targets_a, 1)
-            _, targets_b = torch.max(targets_b, 1)
-            correct += (lam * predicted.eq(targets_a.data).cpu().sum().float() + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
-        else:
-            assert False, "Unknown specificity {}".format(specificity)
-        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / total, 100. * correct / total, correct, total))
+def mixup_data(x, y, device, alpha=1.0):
+    lam = np.random.beta(alpha, alpha) if alpha > 0 else 1
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size).to(device)
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
 
 
-def evaluate(model, device, data_loader, verbose=True):
-    model.eval()
-    loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(data_loader):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            loss += MultiLabelSoftMarginLoss()(output, target).item()
-            predictions = output.argmax(dim=1, keepdim=True)
-            target = target.argmax(dim=1, keepdim=True)
-            correct += predictions.eq(target.view_as(predictions)).sum().item()
-            total += target.size(0)
-            if verbose:
-                progress_bar(batch_idx, len(data_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (loss / total, 100. * correct / total, correct, total))
-    return correct / total, loss / total
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
 
-def full_training(model, train_loader, val_loader, dataset_name, hyperparameters, device, specificity="", vae_model=None):
-    optimizer = optim.Adam(model.parameters(), lr=hyperparameters["learning_rate"])
-    scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-    for epoch in range(hyperparameters["epochs"]):
-        print("Epoch {}/{}".format(epoch, hyperparameters["epochs"]))
-        print("Training ...")
-        train(model, device, train_loader, dataset_name, optimizer, specificity=specificity, vae_model=vae_model, hyperparameters=hyperparameters)
-        print("Evaluation on the validation set ...")
-        evaluate(model, device, val_loader)
-        scheduler.step()
-        print("\n")
+def show_cuda_info():
+    print("torch.cuda.is_available() : {}".format(torch.cuda.is_available()))
+    print("torch.cuda.device_count() : {}".format(torch.cuda.device_count()))
+    print("torch.cuda.current_device() : {}".format(torch.cuda.current_device()))
+    print("torch.cuda.get_device_name(0) : {}".format(torch.cuda.get_device_name(0)))
 
 
-def vae_train(vae, device, optimizer, train_loader):
-    vae.train()
-    train_loss = 0
-    total = 0
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, log_var = vae(data)
-        loss = vae_loss_function(recon_batch, data, mu, log_var, vae.x_dim)
-        loss.backward()
-        train_loss += loss.item()
-        total += target.size(0)
-        optimizer.step()
-        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f' % (train_loss / total))
+def dir_exists(dir_name):  # Returns whether the given name is an existing directory
+    return os.path.isdir(dir_name)
 
 
-def vae_evaluate(vae, device, data_loader, verbose=True):
-    vae.eval()
-    test_loss = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(data_loader):
-            data = data.to(device)
-            recon, mu, log_var = vae(data)
-            total += target.size(0)
-            test_loss += vae_loss_function(recon, data, mu, log_var, vae.x_dim).item()
-            if verbose:
-                progress_bar(batch_idx, len(data_loader), 'Loss: %.3f' % (test_loss / total))
-    return test_loss / total
+def file_exists(file_name):  # Returns whether the given name is an existing file
+    return os.path.isfile(file_name)
 
 
-def full_vae_training(vae, train_loader, val_loader, device, hyperparameters):
-    optimizer = optim.Adam(vae.parameters())
-    for epoch in range(hyperparameters["vae_epochs"]):
-        print("Epoch {}/{}".format(epoch, hyperparameters["vae_epochs"]))
-        print("Training ...")
-        vae_train(vae, device, optimizer, train_loader)
-        print("Evaluation on the validation set ...")
-        vae_evaluate(vae, device, val_loader, verbose=True)
-        print("\n")
+def create_directory(directory_name):  # Creates a new directory given the name if possible
+    try:
+        os.mkdir(directory_name)
+    except FileExistsError:
+        pass
 
 
-def score_report(model, device, val_loader, test_loader, blurred_test_loader):
-    report = [['type', 'accuracy', 'loss', 'deep_fool']]
-    print("Final report :")
-    val_accuracy, val_loss = evaluate(model, device, val_loader, verbose=False)
-    val_df_score = deepfool_score(model, device, val_loader)
-    report.append(["val", val_accuracy, val_loss, val_df_score])
-    print("\tPerformance on the validation set - acc. : {:.3f}, loss : {:.3f}, DeepFool score : {:.3f}".format(val_accuracy, val_loss, val_df_score))
-    test_accuracy, test_loss = evaluate(model, device, test_loader, verbose=False)
-    test_df_score = deepfool_score(model, device, test_loader)
-    report.append(["test", test_accuracy, test_loss, test_df_score])
-    print("\tPerformance on the testing set - acc. : {:.3f}, loss : {:.3f}, DeepFool score : {:.3f}".format(test_accuracy, test_loss, test_df_score))
-    blurred_test_accuracy, blurred_test_loss = evaluate(model, device, blurred_test_loader, verbose=False)
-    blurred_test_df_score = deepfool_score(model, device, blurred_test_loader)
-    report.append(["blurred_test", blurred_test_accuracy, blurred_test_loss, blurred_test_df_score])
-    print("\tPerformance on the blurred testing set - acc. : {:.3f}, loss : {:.3f}, DeepFool score : {:.3f}".format(blurred_test_accuracy, blurred_test_loss, blurred_test_df_score))
-    return report
+def write_csv(file_name, data):
+    with open(file_name, 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        for row in data:
+            writer.writerow(row)
 
 
-def deepfool_score(model, device, test_loader):
-    model.softmax = nn.Identity()
-    # test the model on adversarial examples
-    norms = []
-    batches_done = 0
-    for batch_idx, (data, target) in enumerate(test_loader):
-        data, target = data.to(device), target.to(device)
-        for image, t in zip(data, target):
-            t = t.unsqueeze(0)
-            # generate adversarial examples
-            data = deepfool(image, model, num_classes=10, overshoot=0.02, max_iter=50)
-            minimal_perturbation = data[0]
-            # calculate the norm of the perturbation
-            norm = np.linalg.norm(minimal_perturbation)
-            norms.append(norm)
-        batches_done += 1
-        if batches_done % 50 == 0:
-            break
-    return np.mean(norms)
+def read_csv(file_name):
+    with open(file_name, 'r') as f:
+        csv_reader = csv.reader(f)
+        for row in csv_reader:
+            print(row)
+
+
+_, term_width = 0, 80  # os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+TOTAL_BAR_LENGTH = 40
+last_time = time.time()
+begin_time = last_time
+
+
+# Source : https://github.com/facebookresearch/mixup-cifar10
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+# Source : https://github.com/facebookresearch/mixup-cifar10
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
+
+
+def mask_dict(initial_dict, keys):
+    return {k: initial_dict[k] for k in initial_dict if k in keys}
+
+
+def hyperparameter_to_name(hyperparameters):
+    return "&".join(sorted(["{}:{}".format(k, hyperparameters[k]) for k in hyperparameters]))
+
+
+def report_results():
+    print("Showing the results stored on disk")
+    for dataset in ["mnist", "fashionmnist", "cifar10", "cifar100"]:
+        for variant in ["standard", "mixup", "manifold_mixup", "vae"]:
+            dir_name = "./results/{}/{}".format(dataset, variant)
+            if dir_exists(dir_name):
+                dir_list = os.listdir(dir_name)
+                if len(dir_list) > 0:
+                    print("\n---Results for dataset {} and variant {}---".format(dataset, variant))
+                runs_aggregations = dict()
+                for result_file in dir_list:
+                    seed_index = result_file.find("random_seed")
+                    partial_file_name = result_file[:seed_index-1] + result_file[seed_index+13:]
+                    seed = int(result_file[seed_index+12])
+                    with open("{}/{}".format(dir_name, result_file), newline='') as csvfile:
+                        report = np.zeros((3, 3))
+                        line = 0
+                        for row in csv.reader(csvfile):
+                            if line > 0:
+                                report[line-1, :] = np.array([float(f) for f in row[1:]])
+                            line += 1
+                    if partial_file_name not in runs_aggregations:
+                        runs_aggregations[partial_file_name] = []
+                    runs_aggregations[partial_file_name].append((seed, report))
+                full_reports = []
+                for run in runs_aggregations:
+                    seeds, reports = zip(*runs_aggregations[run])
+                    reports = np.array(reports)
+                    seeds = [str(i) for i in sorted(list(seeds))]
+                    full_reports.append((run, seeds, reports.mean(axis=0), reports.std(axis=0)))
+                full_reports = sorted(full_reports, key=lambda r: -r[2][0][0])  # Sort with respect to the mean validation accuracy
+                for index, report in enumerate(full_reports):
+                    print("Rank {}".format(index))
+                    print("\tParameters : {}".format(report[0][:-4].replace("&", ", ").replace(":", ": ")))
+                    print("\tSeeds : {} (n: {})".format(", ".join(report[1]), len(report[1])))
+                    for i, split in enumerate(["Val", "Test", "Blurred test"]):
+                        for j, metric in enumerate(["accuracy", "loss", "DeepFool score"]):
+                            print("\t{} {} : {:.3f} (+/- {:.3f})".format(split, metric, report[2][i, j], report[3][i, j]))
+
+'''
+# Check some sample to validate the VAE or GAN
+with torch.no_grad():
+    z = torch.randn(64, relevant_hyperparameters["vae_z_dim"]).cuda()
+    sample = vae_model.decoder(z).cuda()
+    save_image(sample.view(64, DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name]), "./sample_vae_" + dataset_name + ".png")
+
+for batch_idx, (data, target) in enumerate(train_loader):
+    if batch_idx > 0:
+        break
+    data, target = data.to(device), target.to(device)
+    flat_data = data.view(-1, vae_model.x_dim)
+    latent_codes = vae_model.encoder(flat_data)[0]# .cpu().detach().numpy()
+    new_images = vae_model.decoder(latent_codes)
+    #save_image(data.view(16, DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name]), "./sample_ori_{}.png".format(batch_idx))
+    #save_image(new_images.view(16, DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name]), "./sample_gen_{}.png".format(batch_idx))
+'''
