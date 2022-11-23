9a2749d30c9cb3bcae7d92d33ebd1426c3f20dcf
tristangirard <82379010+tristangirard@users.noreply.github.com>
2022-11-23 10:36:23 +0100

    Update main.py

--- refactor/main.py
+++ refactor/main.py
@@ -1,10 +1,9 @@
 from torch import optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-
 from data import DATASETS_CALLS, download_dataset, blur_images, load_dataset, DATASET_IMAGE_CHN, DATASET_IMAGE_DIM
 from model import get_standard_model, get_vae
-from train_test import full_training, full_vae_training
+from train_test import full_training, full_vae_training, score_report
 from utils import *
 import torch
 import argparse
@@ -12,22 +11,30 @@ import argparse
 """
 To do :
         - Include GAN
-        - Add the Manifold-Mixup baseline
-        - Save the CIFAR-10 images and VAE generated images on disk to see how the VAE performs
-        - Find good networks in order to achieve near-SOTA accuracy on the standard variant
-        - Add a command to read all the previous results and return the best hyperparameters
+        - Save the CIFAR-10 images and VAE generated images on disk to see how the VAE performs (seems to be a bug with CIFAR-10)
+        - Find good networks in order to achieve near-SOTA accuracy on standard training
+        - Start the hyper-parameter search
 """
+
+# Hyperparameter that affect the training of the different variants
 RELEVANT_HYPERPARAMETER_NAMES = {
     "standard": ["epochs", "batch_size", "learning_rate", "random_seed"],
     "mixup": ["epochs", "batch_size", "learning_rate", "random_seed"],
-    "vae": ["epochs", "batch_size", "learning_rate", "random_seed", "vae_epochs", "vae_h_dim1", "vae_h_dim2", "vae_z_dim", "vae_lat_opt_steps"]
+    "manifold_mixup": ["epochs", "batch_size", "learning_rate", "random_seed"],
+    "vae": ["epochs", "batch_size", "learning_rate", "random_seed", "vae_epochs", "vae_h_dim1", "vae_h_dim2", "vae_z_dim", "vae_lat_opt_steps"],
 }
+
+DATASETS = ["mnist", "fashionmnist", "cifar10", "cifar100"]
+VARIANTS = ["standard", "mixup", "manifold_mixup", "vae"]
 
 parser = argparse.ArgumentParser(description="Experiment for the DeepLearning project")
 
-parser.add_argument('--download_only', '-d', action='store_true', help='only downloads the datasets')
-parser.add_argument('--dataset', choices=["mnist", "fashionmnist", "cifar10", "cifar100"], default="mnist", help="dataset to run the experiment on")
-parser.add_argument('--variant', choices=["standard", "mixup", "vae"], default="standard", help="training and model variant used")
+# Utility commands
+parser.add_argument('--download', '-d', action='store_true', help='only downloads the datasets')
+parser.add_argument('--results', '-r', action='store_true', help='show the results stored on disk')
+
+parser.add_argument('--dataset', choices=DATASETS, default="mnist", help="dataset to run the experiment on")
+parser.add_argument('--variant', choices=VARIANTS, default="standard", help="training and model variant used")
 parser.add_argument('--epochs', type=int, default=10, help="total epochs to run")
 parser.add_argument('--batch_size', type=int, default=16, help="batch size")
 parser.add_argument('--learning_rate', type=float, default=0.001, help="learning rate")
@@ -37,11 +44,14 @@ parser.add_argument('--vae_h_dim1', type=int, default=512, help="first hidden di
 parser.add_argument('--vae_h_dim2', type=int, default=512, help="second hidden dimension of the VAE")
 parser.add_argument('--vae_z_dim', type=int, default=16, help="dimension of the latent code for the VAE")
 parser.add_argument('--vae_lat_opt_steps', type=int, default=0, help="number of steps of the optimization of the latent codes of the VAE")
-# parser.add_argument('--other', nargs='+', type=str, default=[])
 
 args = parser.parse_args()
 
-if args.download_only:
+if args.results:
+    report_results()
+    exit()
+
+if args.download:
     print("Downloading all the datasets")
     for dataset_name in DATASETS_CALLS.keys():
         download_dataset(dataset_name)
@@ -53,6 +63,7 @@ if args.download_only:
 dataset_name = args.dataset
 variant = args.variant
 
+# Make sure that the dataset is stored on disk
 download_dataset(dataset_name)
 blur_images(dataset_name)
 
@@ -94,18 +105,16 @@ For MNIST : batch_size=16, learning_rate=0.001
 
 torch.manual_seed(relevant_hyperparameters["random_seed"])
 
-download_dataset(dataset_name)
-blur_images(dataset_name)
-
 device_name = "cuda" if torch.cuda.is_available() else "cpu"
 print("Device : {}".format(device_name.upper()))
 device = torch.device(device_name)
 
-
+# Load the datasets
 train_dataset, val_dataset = load_dataset(dataset_name, train_val=True)
 test_dataset = load_dataset(dataset_name, train_val=False)
 blurred_test_dataset = load_dataset(dataset_name, train_val=False, specificity="blurred")
 
+# Create the loaders
 train_loader = DataLoader(train_dataset, batch_size=relevant_hyperparameters["batch_size"], shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=100, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False)
@@ -113,14 +122,19 @@ blurred_test_loader = DataLoader(blurred_test_dataset, batch_size=100, shuffle=F
 
 if variant == "standard":
     print("\n\nTraining with the standard model")
-    model = get_standard_model(dataset_name)
+    model = get_standard_model(dataset_name, device)
     model = model.to(device)
-    full_training(model, train_loader, val_loader, relevant_hyperparameters, device)
+    full_training(model, train_loader, val_loader, dataset_name, relevant_hyperparameters, device)
 elif variant == "mixup":
     print("\n\nTraining with Mixup")
-    model = get_standard_model(dataset_name)
+    model = get_standard_model(dataset_name, device)
     model = model.to(device)
-    full_training(model, train_loader, val_loader, relevant_hyperparameters, device, specificity="mixup")
+    full_training(model, train_loader, val_loader, dataset_name, relevant_hyperparameters, device, specificity="mixup")
+elif variant == "manifold_mixup":
+    print("\n\nTraining with Manifold Mixup")
+    model = get_standard_model(dataset_name, device)
+    model = model.to(device)
+    full_training(model, train_loader, val_loader, dataset_name, relevant_hyperparameters, device, specificity="manifold_mixup")
 elif variant == "vae":
     print("\n\nTraining with a VAE")
     vae_relevant_hyperparameters = mask_dict(relevant_hyperparameters, ["vae_h_dim1", "vae_h_dim2", "vae_z_dim", "vae_epochs", "random_seed"])
@@ -131,9 +145,10 @@ elif variant == "vae":
     vae_file_name = "./models/{}/vae/{}.pth".format(dataset_name, vae_hyperparameter_name)
     vae_latent_code_file_name = "./augmented_datasets/{}/{}_train_latent_codes.npy".format(dataset_name, vae_latent_code_hyperparameter_name)
     vae_latent_labels_file_name = "./augmented_datasets/{}/{}_train_latent_labels.npy".format(dataset_name, vae_latent_code_hyperparameter_name)
-
+    # Get the VAE model
     vae_model = get_vae(dataset_name, h_dim1=relevant_hyperparameters["vae_h_dim1"], h_dim2=relevant_hyperparameters["vae_h_dim2"], z_dim=relevant_hyperparameters["vae_z_dim"])
     vae_model = vae_model.to(device)
+    # Train it or load it from disk if possible
     if file_exists(vae_file_name):
         print("VAE model is already on disk")
         vae_model.load_state_dict(torch.load(vae_file_name))
@@ -141,24 +156,7 @@ elif variant == "vae":
         print("Training the VAE model")
         full_vae_training(vae_model, train_loader, val_loader, device, relevant_hyperparameters)
         torch.save(vae_model.state_dict(), vae_file_name)
-    '''
-    # Check some sample to validate the VAE
-    with torch.no_grad():
-        z = torch.randn(64, relevant_hyperparameters["vae_z_dim"]).cuda()
-        sample = vae_model.decoder(z).cuda()
-        save_image(sample.view(64, DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name]), "./sample_vae_" + dataset_name + ".png")
-
-    for batch_idx, (data, target) in enumerate(train_loader):
-        if batch_idx > 0:
-            break
-        data, target = data.to(device), target.to(device)
-        flat_data = data.view(-1, vae_model.x_dim)
-        latent_codes = vae_model.encoder(flat_data)[0]# .cpu().detach().numpy()
-        new_images = vae_model.decoder(latent_codes)
-        #save_image(data.view(16, DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name]), "./sample_ori_{}.png".format(batch_idx))
-        #save_image(new_images.view(16, DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name]), "./sample_gen_{}.png".format(batch_idx))
-    '''
-    # Generate the precise latent codes for each image
+    # Generate the latent codes for each image of the training set or load the codes if possible
     if file_exists(vae_latent_code_file_name) and file_exists(vae_latent_labels_file_name):
         print("Latent codes of the training set are already stored")
         latent_x = np.load(vae_latent_code_file_name)
@@ -174,6 +172,7 @@ elif variant == "vae":
             latent_codes = torch.from_numpy(latent_codes.cpu().detach().numpy()).float().to(device)
             opt_latent_codes = latent_codes.clone().detach().requires_grad_(True)
             optimizer = optim.Adam([opt_latent_codes], lr=0.1)
+            # Optimize the latent codes in order to reconstruct the image more precisely
             for i in range(vae_latent_code_relevant_hyperparameters["vae_lat_opt_steps"]):
                 optimizer.zero_grad()
                 gen = vae_model.decoder(opt_latent_codes).view(relevant_hyperparameters["batch_size"], DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name])
@@ -185,16 +184,18 @@ elif variant == "vae":
             progress_bar(batch_idx, len(train_loader))
         latent_x = np.concatenate(latent_x, axis=0)
         latent_y = np.concatenate(latent_y, axis=0)
+        # Store the latent codes for later reuse
         np.save(vae_latent_code_file_name, latent_x)
         np.save(vae_latent_labels_file_name, latent_y)
     latent_x = torch.from_numpy(latent_x).float()
     latent_y = torch.from_numpy(latent_y).float()
     latent_train_loader = DataLoader(torch.utils.data.TensorDataset(latent_x, latent_y), batch_size=relevant_hyperparameters["batch_size"], shuffle=True)
-    model = get_standard_model(dataset_name)
+    model = get_standard_model(dataset_name, device)
     model = model.to(device)
-    full_training(model, latent_train_loader, val_loader, relevant_hyperparameters, device, specificity="mixup_vae", vae_model=vae_model)
+    # Train the model with the latent codes
+    full_training(model, latent_train_loader, val_loader, dataset_name, relevant_hyperparameters, device, specificity="mixup_vae", vae_model=vae_model)
 else:
     assert False
 
-# report = score_report(model, device, val_loader, test_loader, blurred_test_loader)
-# write_csv(report_file_name, report)
+report = score_report(model, device, val_loader, test_loader, blurred_test_loader)
+write_csv(report_file_name, report)
