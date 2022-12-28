542c1b58b8958a94d37a257da806cef721884ad2
tristangirard <82379010+tristangirard@users.noreply.github.com>
2022-12-28 14:33:55 +0100

    Added the mixup-ratio argument

--- train_test.py
+++ train_test.py
@@ -1,24 +1,33 @@
+import random
 import numpy as np
 from torch import optim
 from torch.autograd import Variable
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import StepLR
+import torchvision.transforms.functional as F
 from deepfool import deepfool
 from utils import progress_bar, mixup_criterion, mixup_data, DATASET_IMAGE_CHN, DATASET_IMAGE_DIM
 from model import vae_loss_function
 
 
-def train(model, device, train_loader, dataset_name, optimizer, specificity="", mixup_alpha=1.0, vae_model=None):
+def train(model, device, image_train_loader, dataset_name, optimizer, hyperparameters, specificity="", mixup_alpha=1.0, mixup_ratio=1.0, vae_model=None, latent_train_loader=None):
     criterion = nn.CrossEntropyLoss()
     model.train()
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+    loader_size = len(image_train_loader)
+    assert latent_train_loader is None or loader_size == len(latent_train_loader), "Loaders don't have the same size"
+    image_it = iter(image_train_loader)
+    latent_it = None if latent_train_loader is None else iter(latent_train_loader)
+    for batch_idx in range(loader_size):
         optimizer.zero_grad()
-        if specificity == "":
+        image_data, image_target = next(image_it)
+        latent_data, latent_target = (None, None) if latent_it is None else next(latent_it)
+        data, target = image_data, image_target
+        data, target = data.to(device), target.to(device)
+        if specificity == "" or random.random() > mixup_ratio:
             # Standard training
             output = model(data)
             loss = criterion(output, target)
@@ -40,11 +49,15 @@ def train(model, device, train_loader, dataset_name, optimizer, specificity="", 
             total += target.size(0)
             correct += (lam * predicted.eq(targets_a.data).cpu().sum().float() + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
         elif specificity == "mixup_vae":
+            data, target = latent_data, latent_target
+            data, target = data.to(device), target.to(device)
             # Mixup on the latent codes
             assert vae_model is not None, "No VAE model has been provided"
             inputs, targets_a, targets_b, lam = mixup_data(data, target, device=device, alpha=mixup_alpha)
             inputs, targets_a, targets_b = map(Variable, (inputs, targets_a, targets_b))
             inputs = vae_model.decoder(inputs).to(device).view(inputs.shape[0], DATASET_IMAGE_CHN[dataset_name], DATASET_IMAGE_DIM[dataset_name], DATASET_IMAGE_DIM[dataset_name])
+            if hyperparameters["vae_sharp"] > 1:
+                inputs = F.adjust_sharpness(inputs, hyperparameters["vae_sharp"])  # Sharpen the output of the VAE
             outputs = model(inputs)
             loss = mixup_criterion(criterion, outputs, targets_a, targets_b, lam)
             loss.backward()
@@ -55,7 +68,7 @@ def train(model, device, train_loader, dataset_name, optimizer, specificity="", 
             correct += (lam * predicted.eq(targets_a.data).cpu().sum() + (1 - lam) * predicted.eq(targets_b.data).cpu().sum())
         else:
             assert False, "Unknown specificity {}".format(specificity)
-        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / total, 100. * correct / total, correct, total))
+        progress_bar(batch_idx, loader_size, 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / total, 100. * correct / total, correct, total))
 
 
 def evaluate(model, device, data_loader, verbose=True):
@@ -77,19 +90,17 @@ def evaluate(model, device, data_loader, verbose=True):
     return correct / total, loss / total
 
 
-def full_training(model, train_loader, val_loader, dataset_name, hyperparameters, device, specificity="", mixup_alpha=1.0, vae_model=None):
+def full_training(model, image_train_loader, val_loader, dataset_name, hyperparameters, device, specificity="", mixup_alpha=1.0, mixup_ratio=1.0, vae_model=None, latent_train_loader=None):
     optimizer = optim.Adam(model.parameters(), lr=hyperparameters["learning_rate"])
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
     for epoch in range(hyperparameters["epochs"]):
         print("Epoch {}/{}".format(epoch, hyperparameters["epochs"]))
         print("Training ...")
-        train(model, device, train_loader, dataset_name, optimizer, specificity=specificity, mixup_alpha=mixup_alpha, vae_model=vae_model)
+        train(model, device, image_train_loader, dataset_name, optimizer, hyperparameters, specificity=specificity, mixup_alpha=mixup_alpha, mixup_ratio=mixup_ratio, vae_model=vae_model, latent_train_loader=latent_train_loader)
         print("Evaluation on the validation set ...")
         evaluate(model, device, val_loader)
         scheduler.step()
         print("\n")
-        # if specificity == "":
-        #     torch.save(model, "./models/" + str(dataset_name) + "/standard/model.pt")
 
 
 def vae_train(vae, device, optimizer, train_loader):
@@ -105,7 +116,7 @@ def vae_train(vae, device, optimizer, train_loader):
         train_loss += loss.item()
         total += target.size(0)
         optimizer.step()
-        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f' % (train_loss / total))
+        # progress_bar(batch_idx, len(train_loader), 'Loss: %.3f' % (train_loss / total))
 
 
 def vae_evaluate(vae, device, data_loader, verbose=True):
@@ -118,8 +129,8 @@ def vae_evaluate(vae, device, data_loader, verbose=True):
             recon, mu, log_var = vae(data)
             total += target.size(0)
             test_loss += vae_loss_function(recon, data, mu, log_var, vae.x_dim).item()
-            if verbose:
-                progress_bar(batch_idx, len(data_loader), 'Loss: %.3f' % (test_loss / total))
+            # if verbose:
+                # progress_bar(batch_idx, len(data_loader), 'Loss: %.3f' % (test_loss / total))
     return test_loss / total
 
 
